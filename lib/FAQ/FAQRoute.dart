cb5544584b04e23148fad6a60f8ddffb6cda258d
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-05-31 21:55:26 +0200

    ListView for faq

--- lib/FAQ/FAQRoute.dart
+++ lib/FAQ/FAQRoute.dart
@@ -10,12 +10,18 @@ class FAQRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var faqElements = [
+      FAQElement('question',
+          'Long answer. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.')
+    ];
     return Scaffold(
       appBar: NavBar(AppLocalizations.of(context)!.faq),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[FAQElement('title', 'content')],
+        child: ListView.builder(
+          itemCount: faqElements.length,
+          itemBuilder: (BuildContext context, int index) {
+            return faqElements[index];
+          },
         ),
       ),
     );
