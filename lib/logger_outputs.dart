29c1f3d2bfc729ef00d709abbcd9583c83fc3367
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-06-01 20:37:22 +0200

    switch to exclusive in memory logging

--- lib/logger_outputs.dart
+++ lib/logger_outputs.dart
@@ -1,40 +1,5 @@
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:logger/logger.dart';
-
-/// Writes the log output to a file.
-class MyFileOutput extends LogOutput {
-  final File file;
-  final bool overrideExisting;
-  final Encoding encoding;
-  IOSink? _sink;
-
-  MyFileOutput({
-    required this.file,
-    this.overrideExisting = false,
-    this.encoding = utf8,
-  });
-
-  @override
-  void init() {
-    _sink = file.openWrite(
-      mode: overrideExisting ? FileMode.writeOnly : FileMode.writeOnlyAppend,
-      encoding: encoding,
-    );
-  }
-
-  @override
-  void output(OutputEvent event) {
-    _sink?.writeAll(event.lines, '\n');
-  }
-
-  @override
-  void destroy() async {
-    await _sink?.flush();
-    await _sink?.close();
-  }
-}
 
 class RetainOutput extends LogOutput {
   String get content {
