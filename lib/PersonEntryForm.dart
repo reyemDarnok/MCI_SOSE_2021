46aa58f7c2b054ce1f75932eccf3c13143107c3a
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-06-10 16:24:10 +0200

    nicer person entry

--- lib/PersonEntryForm.dart
+++ lib/PersonEntryForm.dart
@@ -46,7 +46,6 @@ class PersonEntryFormState extends State<PersonEntryForm> {
   final String submitText;
 
   final RegExp phoneNumberRegex = RegExp(r'^[+\-()\d\s]+$');
-  final RegExp streetRegex = RegExp(r'^[\w\s.,-]+$');
   final RegExp cityRegex =
       RegExp(r'^(\d{5}[\s,]+[\w-]+|[\w-]+[\s,]+\d{5}|\d{5})$');
 
@@ -62,65 +61,81 @@ class PersonEntryFormState extends State<PersonEntryForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
+        child: Center(
+            child: SizedBox(
+          width: MediaQuery.of(context).size.width * 2 / 3,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context)!.nameHintText,
+                    labelText: AppLocalizations.of(context)!.nameLabel),
+                controller: name,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return AppLocalizations.of(context)!.nameMissingError;
+                  } else {
+                    return null;
+                  }
+                },
+              ),
           TextFormField(
-            controller: name,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return AppLocalizations.of(context)!.nameMissingError;
-              } else {
-                return null;
-              }
-            },
-          ),
+                  decoration: InputDecoration(
+                      hintText:
+                          AppLocalizations.of(context)!.phoneNumberHintText,
+                      labelText:
+                          AppLocalizations.of(context)!.phoneNumberLabel),
+                  controller: telephoneNumber,
+                  validator: (value) {
+                    if (value == null) {
+                      return AppLocalizations.of(context)!
+                          .phoneNumberMissingError;
+                    } else if (!phoneNumberRegex.hasMatch(value)) {
+                      return AppLocalizations.of(context)!
+                          .phoneNumberMalformedError;
+                    } else {
+                      return null;
+                    }
+                  }),
           TextFormField(
-              controller: telephoneNumber,
-              validator: (value) {
-                if (value == null) {
-                  return AppLocalizations.of(context)!.phoneNumberMissingError;
-                } else if (!phoneNumberRegex.hasMatch(value)) {
-                  return AppLocalizations.of(context)!
-                      .phoneNumberMalformedError;
-                } else {
-                  return null;
-                }
-              }),
+                decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context)!.streetHintText,
+                    labelText: AppLocalizations.of(context)!.streetLabel),
+                controller: street,
+                validator: (value) {
+                  if (value == null) {
+                    return AppLocalizations.of(context)!.streetMissingError;
+                  } else {
+                    return null;
+                  }
+                },
+              ),
           TextFormField(
-            controller: street,
-            validator: (value) {
-              if (value == null) {
-                return AppLocalizations.of(context)!.streetMissingError;
-              } else if (!streetRegex.hasMatch(value)) {
-                return AppLocalizations.of(context)!.streetMalformedError;
-              } else {
-                return null;
-              }
-            },
-          ),
-          TextFormField(
-            controller: city,
-            validator: (value) {
-              if (value == null) {
-                return AppLocalizations.of(context)!.cityMissingError;
-              } else if (!cityRegex.hasMatch(value)) {
-                return AppLocalizations.of(context)!.cityMalformedError;
-              }
-            },
-          ),
+                decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context)!.cityHintText,
+                    labelText: AppLocalizations.of(context)!.cityLabel),
+                controller: city,
+                validator: (value) {
+                  if (value == null) {
+                    return AppLocalizations.of(context)!.cityMissingError;
+                  } else if (!cityRegex.hasMatch(value)) {
+                    return AppLocalizations.of(context)!.cityMalformedError;
+                  }
+                },
+              ),
           ElevatedButton(
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
-                  callback(Person(
-                      name: name.text,
-                      telephone: telephoneNumber.text,
-                      street: street.text,
-                      city: city.text));
-                }
-              },
-              child: Text(submitText))
-        ],
-      ),
-    );
+                      callback(Person(
+                          name: name.text,
+                          telephone: telephoneNumber.text,
+                          street: street.text,
+                          city: city.text));
+                    }
+                  },
+                  child: Text(submitText))
+            ],
+          ),
+        )));
   }
 }
