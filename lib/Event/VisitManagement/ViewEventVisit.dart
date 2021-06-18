a755f7f020d9203bdf027bb89ad6e2c5b6a305a7
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-06-18 15:23:44 +0200

    make eventVisit nice

--- lib/Event/VisitManagement/ViewEventVisit.dart
+++ lib/Event/VisitManagement/ViewEventVisit.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:mci_practicum/miscTypes/EventVisit.dart';
 
@@ -12,18 +14,19 @@ class ViewEventVisitRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     var dateFormat = DateFormat.Hms(appLocale.value?.toLanguageTag()).add_yMd();
     final args = ModalRoute.of(context)!.settings.arguments
-        as ViewEventVisitRouteArguments;
+    as ViewEventVisitRouteArguments;
     return Scaffold(
         appBar: NavBar(args.eventVisit.event.name),
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-              //TODO make nice
-              Text(args.eventVisit.event.name),
-              Text(dateFormat.format(args.eventVisit.start)),
-              Text(dateFormat.format(
-                  args.eventVisit.start.add(args.eventVisit.visitDuration)))
+              Text(AppLocalizations.of(context)!
+                  .name(args.eventVisit.event.name)),
+              Text(AppLocalizations.of(context)!
+                  .start(dateFormat.format(args.eventVisit.start))),
+              Text(AppLocalizations.of(context)!.end(dateFormat.format(
+                  args.eventVisit.start.add(args.eventVisit.visitDuration))))
             ])));
   }
 }
